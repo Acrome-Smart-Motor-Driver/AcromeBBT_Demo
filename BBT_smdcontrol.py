f835447e57ff0a023957a2869571e12b1dcaa1b1
Serhan Argun <30629499+serhargun@users.noreply.github.com>
2023-06-27 17:05:20 +0300

    Update BBT_smdcontrol.py
    Comments are added

--- BBT_smdcontrol.py
+++ BBT_smdcontrol.py
@@ -1,4 +1,4 @@
-from smd.smd import Master, Index
+# This code is importing necessary modules and classes for the program to run.
 import socket
 import copy
 import sys
@@ -7,11 +7,12 @@ import struct
 import time
 import concurrent.futures as cf
 
+from smd.smd import Master, Index
+
 from queue import Queue
-
-
 s_q = Queue(1)
 
+# The Server class creates a socket and receives data from a client.
 class Server():
     IP = '127.0.0.1'
     bufferSize = 4
@@ -30,6 +31,16 @@ class Server():
 
 
 def loop_server(s: Server):
+    """
+    The function continuously receives position data from a server and shares it with a master via a
+    queue.
+    
+    :param s: The parameter "s" is an instance of the Server class, which is likely used for
+    communication between different components or systems in a larger program or network. The
+    "loop_server" function continuously receives data from this server and puts it into a queue for
+    further processing
+    :type s: Server
+    """
     while True:
         data = None
         try:
@@ -47,6 +58,16 @@ def loop_server(s: Server):
 
 
 def loop_smd(m: Master, s: Server):
+    """
+    The function continuously reads joystick data and sends it to a server while also receiving data
+    from the server to control two actuators.
+    
+    :param m: The variable `m` is an instance of the `Master` class, which is likely used for
+    communication with some sort of hardware device or system
+    :type m: Master
+    :param s: The variable `s` is an instance of the `Server` class
+    :type s: Server
+    """
     while True:
 
         m.send(m.Actuators[0].Read(params=[Index.JoystickX, Index.JoystickY]))
@@ -87,6 +108,10 @@ def loop_smd(m: Master, s: Server):
             time.sleep(0.0015)
 
 
+# This code is trying to assign the string 'COM5' to the variable `smd_port`. If an `IndexError` is
+# raised, it means that the user did not provide the correct number of command line arguments, and the
+# program will print a message asking the user to enter the correct number of arguments. If any other
+# exception is raised, it will be re-raised to the calling function.
 try:
     smd_port = 'COM5'
 except IndexError:
@@ -95,6 +120,7 @@ except Exception as exc:
     raise exc
 
 
+# This code is initializing several objects and variables.
 m = Master(4096, smd_port)
 s_in = Server(8000)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -104,6 +130,11 @@ sock.settimeout(0.001)
 print(m.AutoScan())
 
 
+# This code is creating a thread pool executor with a maximum of 6 worker threads. It then submits two
+# functions, `loop_server` and `loop_smd`, to the executor to be run in separate threads. The
+# `loop_server` function continuously receives position data from a server and puts it into a queue
+# for further processing, while the `loop_smd` function continuously reads joystick data and sends it
+# to a server while also receiving data from the server to control two actuators.
 with cf.ThreadPoolExecutor(max_workers=6) as executor:
     s_exec = executor.submit(loop_server, s_in)
     m_exec = executor.submit(loop_smd, m, s_in)
